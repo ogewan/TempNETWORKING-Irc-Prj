167fd2a4ae82fe234846d684aa5bdd1002fc214f
seun40 <seun40@gmail.com>
2013-04-08 20:21:10 -0500

    Server send/recieve complete; irc logic next

--- p1a_actual.c
+++ p1a_actual.c
@@ -9,39 +9,73 @@
 /*/cygdrive/c/Users/Dolapo/Documents/GitHub/chirc/src*/
 int main()
 {
-	int ssckt, csckt;
+	/*Variable Initialization*/
+	int ssckt;
+	int csckt = 0;
 	struct sockaddr_in sadr, cadr;
 	int yes = 1;
 	socklen_t sktsz = sizeof(struct sockaddr_in);
 
-	char* welcome = "Welcome to the Internet Relay Network";
-	int port = 8998
+	char* RPL_WELCOME = "Welcome to the Internet Relay Network";
+	int port = 8998;
 	char* nicks[20];
 	char* users[20];
 	int usrcount = 0;
-	char* hostname;
+	char hostname[128];
+	char peer[128];
+	char buf[1024];
+	int bytes = 0;
 	/*:hostname 001 user1 :*/
 	/* user1!user1@foo.example.com\r\n*/
-	<nick>!<user>@<host>
-	if (gethostname(hostname, size_t sizeof(hostname)))
-		{
-			hostname = "testdefault";
-			sethostname(hostname, size_t sizeof(hostname)))
-		}
-	memset(&serverAddr, 0, sizeof(sadr));
+	/*:hostname RPL_WELCOME-numericcode nick :*/
+	/*<nick>!<user>@<host(peer)>\r\n*/
+		
+	int hostattmpt = gethostname(hostname, sizeof(hostname));
+	if (hostattmpt)/*Fail to get hostname? Make one up! :D*/
+	{
+		char* temphostname = "testdefault";
+		sethostname(temphostname, sizeof(temphostname));
+	}
+	/*Server Socket Setup*/
+	memset(&sadr, 0, sizeof(sadr));
 	sadr.sin_family = AF_INET;
 	sadr.sin_port = htons(port);
 	sadr.sin_addr.s_addr = INADDR_ANY;
-
 	ssckt = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
 	setsockopt(ssckt, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
 	bind(ssckt, (struct sockaddr *) &sadr, sizeof(sadr));
 	listen(ssckt, 10);
-
+	/*END*/
+	
+	printf("Initializing Server:%s\n",hostname);
+	
 	while(1)
 	{
-		csckt = accept(ssckt, (struct sockaddr *) &cadr, &sktsz);
-		send(csckt, response, strlen(response), 0);/*Send response to client*/
+		if (!csckt)
+		{
+			csckt = accept(ssckt, (struct sockaddr *) &cadr, &sktsz);
+			getnameinfo(&cadr, sizeof(cadr),peer, sizeof(peer), NULL, NULL, 0);
+		}
+		bytes = recv(csckt, buf, sizeof(buf),0);
+		if (bytes)
+		{
+			printf("Recieved\n");
+			char jubby[1028];
+			sprintf(jubby, "%d", bytes);	
+			send(jubby, peer, strlen(jubby), 0);
+		}
+		/*Ignore every response other than Nick and User
+		/*csckt = accept(ssckt, (struct sockaddr *) &cadr, &sktsz);
+		/*get peername
+		check for peername in users list
+		if in userslist:
+			take in input and respond\
+		else:
+			look for user and nick commands
+		/*/			
+		char* res =  "RESPONSE\n";
+		/*send(res, peer, strlen(res), 0);/*Send response to client*/
+		printf("connected:%s %d\n", peer,bytes);
 	}
 
 	close(ssckt);
